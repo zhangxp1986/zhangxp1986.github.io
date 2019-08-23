2e585893a8c3f4e7fa1db2e5b25436febf75f90e
zhangxp1986 <54387236+zhangxp1986@users.noreply.github.com>
2019-08-24 03:35:56 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+let myImage = document.querySelector('img');
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
+
+function setHeading(name) {
+    let myHeading = document.querySelector('h1');
+    myHeading.textContent = 'Mozilla 酷毙了，' + name + '！';
+}
+
+function setUserName() {
+    let myName = prompt('请输入你的名字');
+    sessionStorage.setItem('name', myName);
+    setHeading(myName);
+}
+
+let storedName = sessionStorage.getItem('name');
+if(!storedName) {
+    setUserName();
+} else {
+    setHeading(storedName);
+}
+
+let myButton = document.querySelector('button');
+myButton.onclick = setUserName;
